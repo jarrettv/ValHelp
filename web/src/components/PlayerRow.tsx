5c505e0b11d966376e5ee30342c91c4ca01aff5e
Jarrett Vance <jarrettv@gmail.com>
2025-02-08 11:19:59 -0600

    Watch live

--- web/src/components/PlayerRow.tsx
+++ web/src/components/PlayerRow.tsx
@@ -6,6 +6,7 @@ import { PlayerPenalty } from './PlayerPenalty';
 import { useAuth } from '../contexts/AuthContext';
 import Youtube from './Youtube';
 import Twitch from './Twitch';
+import Watch from './Watch';
 
 interface PlayerRowProps {
   player: Player;
@@ -22,18 +23,17 @@ const PlayerRow: React.FC<PlayerRowProps> = ({ player, mode }) => {
     <div className={`player-row ${playerStatus}`}>
       <div className="player-info">
         <img src={player.avatarUrl} alt={player.name} className="player-avatar" />
-        {player.stream.length > 5 && <div className="player-name"><a title="Watch Stream" href={player.stream} target="_blank">{player.name}</a></div> }
-        {player.stream.length <= 5 && <div className="player-name">{player.name}</div> }
+        <div className="player-name">{player.name}</div>
         {mode === "logs" && <div className="player-score">{player.score}</div>}
       </div>
       {mode === "info" && <div className="player-info">
         {player.logs.filter(log => log.code.startsWith("Stream")).map((log) => (
           <div key={log.code} style={{fontSize:'0.8rem'}}>
-            {log.code.startsWith("StreamYoutube") && <a href={log.code.split('=')[1].split(',')[1]} target="_blank"><Youtube width="30" height="30" style={{ verticalAlign: "middle", marginRight:'0.2rem' }} />{log.code.split('=')[1].split(',')[0]}</a>}
-            {log.code.startsWith("StreamTwitch") && <a href={log.code.split('=')[1].split(',')[1]} target="_blank"><Twitch width="30" height="30" style={{ verticalAlign: "middle", marginRight:'0.2rem' }} />{log.code.split('=')[1].split(',')[0]}</a>}
+            {log.code.startsWith("StreamYoutube") && <a href={log.code.split('=')[1].split(',')[1]} target="_blank"><Youtube width="30" height="30" style={{ verticalAlign: "middle", margin:'0 0.3rem' }} /><span>{log.code.split('=')[1].split(',')[0]}</span></a>}
+            {log.code.startsWith("StreamTwitch") && <a href={log.code.split('=')[1].split(',')[1]} target="_blank"><Twitch width="30" height="30" style={{ verticalAlign: "middle", margin:'0 0.3rem' }} /><span>{log.code.split('=')[1].split(',')[0]}</span></a>}
           </div>
         ))}
-        {player.logs.filter(log => log.code.startsWith("Stream")).length === 0 && player.stream.length > 5 && <a href={player.stream} target="_blank">Watch Stream</a>}
+        {player.logs.filter(log => log.code.startsWith("Stream")).length === 0 && player.stream.length > 5 && <a href={player.stream} target="_blank"><Watch width="30" height="30" style={{ color:"#5b6eae", verticalAlign: "middle", margin:'0 0.7rem' }} />Watch</a>}
       </div>}
       {mode === "logs" && <div className="player-logs">
         {player.logs.map((log, index) =>
