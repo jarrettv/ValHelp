96bd145969b7fe02ccb04ee322b2d757c514b45b
Jarrett V <jarrettv@gmail.com>
2025-12-27 13:06:51 -0600

    Rusti cali suggested tweaks

--- web/src/components/ObsScores.tsx
+++ web/src/components/ObsScores.tsx
@@ -35,8 +35,8 @@ export default function ObsScores(props: ObsScoresProps) {
         { status === "live" && <Countdown targetTime={new Date(props.event.endAt)} color={ props.live ?? '#72da83' } message="OVER" /> }
         { status === "post" && <div className="num countdown over" style={{color: props.post ?? '#9fd2ff'}}><small>{props.event.hours}h&#160;</small>FINAL</div>}
       </div> */}
-      {!props.hideTitle && <div className="obs-title" style={{ color: props.title ?? '#fff' }}>
-        {props.event.name.replace('Trophy ', '').replace('Event ', '').replace('# ', '#').substring(0, 13)}
+      {!props.hideTitle && <div className="obs-title" style={{ color: props.title ?? '#fff', textAlign: 'left' }}>
+        {props.event.name.replace('Trophy ', '').replace('Event ', '').replace('Trailblazer', 'Blazer').replace('Charity', '💝').replace('# ', '#').substring(0, 13)}
       </div>
       }
       {topPlayers.map((player) => (
